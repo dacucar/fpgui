9909bb8fe26b829460f11d1edcffc738485c2102
dacucar <dacucar@dacucar.com>
2016-08-19 00:04:26 +0200

    Add Template of GUI application

---
+++ Src/FpgUI.Wpf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xwt;
+
+namespace FpgUI.Wpf
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main ( string [] args )
+        {
+            Application.Initialize ( ToolkitType.Wpf );
+
+            MainWindow w = new MainWindow ();
+            w.Show ();
+
+            Application.Run ();
+
+            w.Dispose ();
+            Application.Dispose ();
+        }
+    }
+}
+
---
+++ Src/FpgUI/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using Xwt;
+
+namespace FpgUI
+{
+    public class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            Title = "FpgUI";
+            Width = 400;
+            Height = 400;
+
+            Menu menu = new Menu ();
+
+            var file = new MenuItem ( "_File" );
+            file.SubMenu = new Menu ();
+            file.SubMenu.Items.Add ( new MenuItem ( "_Open" ) );
+            file.SubMenu.Items.Add ( new MenuItem ( "_New" ) );
+            MenuItem mi = new MenuItem ( "_Close" );
+            mi.Clicked += (sender, e ) => Close ();
+            file.SubMenu.Items.Add ( mi );
+            menu.Items.Add ( file );
+            MainMenu = menu;
+
+            var sampleLabel = new Label ( "FpgUI" );
+            Content = sampleLabel;
+        }
+
+        protected override bool OnCloseRequested ()
+        {
+            var allow_close = MessageDialog.Confirm ( "FpgUI will be closed", Command.Ok );
+            if ( allow_close )
+                Application.Exit ();
+            return allow_close;
+        }
+    }
+}
+
+
