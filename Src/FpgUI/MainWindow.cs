2464dc93ee9212f86600cec4dcd8d7f70a1d799d
dacucar <dacucar@dacucar.com>
2016-10-23 12:42:11 +0200

    add palette menu

--- Src/FpgUI/MainWindow.cs
+++ Src/FpgUI/MainWindow.cs
@@ -2,77 +2,86 @@
 
 namespace FpgUI
 {
-    public class MainWindow : Window
-    {
-        public MainWindow ()
-        {
-            Title = "FpgUI";
-            Width = 400;
-            Height = 400;
+	public class MainWindow : Window
+	{
+		public MainWindow ()
+		{
+			Title = "FpgUI";
+			Width = 400;
+			Height = 400;
 
 			Menu mainMenu = new Menu ();
 
-            var fileMenu = new MenuItem ( "_File" );
-            fileMenu.SubMenu = new Menu ();
-            fileMenu.SubMenu.Items.Add ( new MenuItem ( "_Open" ) );
-            fileMenu.SubMenu.Items.Add ( new MenuItem ( "_New" ) );
-            MenuItem mi = new MenuItem ( "_Close" );
-            mi.Clicked += (sender, e ) => Close ();
-            fileMenu.SubMenu.Items.Add ( mi );
+			var fileMenu = new MenuItem ( "_File" );
+			fileMenu.SubMenu = new Menu ();
+			fileMenu.SubMenu.Items.Add ( new MenuItem ( "_Open" ) );
+			fileMenu.SubMenu.Items.Add ( new MenuItem ( "_New" ) );
+			MenuItem mi = new MenuItem ( "_Close" );
+			mi.Clicked += (sender, e ) => Close ();
+			fileMenu.SubMenu.Items.Add ( mi );
 
 			var editMenu = new MenuItem ( "_Edit" );
-			var cutEdit = new MenuItem ("_Cut");
-			var copyEdit = new MenuItem ("_Copy");
-			var pasteEdit = new MenuItem ("_Paste");
+			var cutEdit = new MenuItem ( "_Cut" );
+			var copyEdit = new MenuItem ( "_Copy" );
+			var pasteEdit = new MenuItem ( "_Paste" );
 
 			editMenu.SubMenu = new Menu ();
-			editMenu.SubMenu.Items.Add (cutEdit);
-			editMenu.SubMenu.Items.Add (copyEdit);
-			editMenu.SubMenu.Items.Add (pasteEdit);
+			editMenu.SubMenu.Items.Add ( cutEdit );
+			editMenu.SubMenu.Items.Add ( copyEdit );
+			editMenu.SubMenu.Items.Add ( pasteEdit );
+
+			var paletteMenu = new MenuItem ( "_Palette" );
+			var viewEditPalette = new MenuItem ( "_View/Edit..." );
+			var exportPalette = new MenuItem ( "_Export to PAL..." );
+
+			paletteMenu.SubMenu = new Menu ();
+			paletteMenu.SubMenu.Items.Add ( viewEditPalette );
+			paletteMenu.SubMenu.Items.Add ( exportPalette );
 
 			var graphicMenu = new MenuItem ( "_Graphic" );
-			var addGraphic = new MenuItem ("_Add");
-			var editGraphic = new MenuItem ("_View/Edit");
-			var extractGraphic = new MenuItem ("_Extract");
-			var extractToMap = new MenuItem ("To _MAP file...");
-			var extractToPng = new MenuItem ("To _PNG file...");
-			var deleteGraphic = new MenuItem ("_Delete");
+			var addGraphic = new MenuItem ( "_Add" );
+			var editGraphic = new MenuItem ( "_View/Edit" );
+			var extractGraphic = new MenuItem ( "_Extract" );
+			var extractToMap = new MenuItem ( "To _MAP file..." );
+			var extractToPng = new MenuItem ( "To _PNG file..." );
+			var deleteGraphic = new MenuItem ( "_Delete" );
 
 			extractGraphic.SubMenu = new Menu ();
-			extractGraphic.SubMenu.Items.Add (extractToMap);
-			extractGraphic.SubMenu.Items.Add (extractToPng);
+			extractGraphic.SubMenu.Items.Add ( extractToMap );
+			extractGraphic.SubMenu.Items.Add ( extractToPng );
 	
 			graphicMenu.SubMenu = new Menu ();
-			graphicMenu.SubMenu.Items.Add (addGraphic);
-			graphicMenu.SubMenu.Items.Add (editGraphic);
-			graphicMenu.SubMenu.Items.Add (extractGraphic);
-			graphicMenu.SubMenu.Items.Add (deleteGraphic);
+			graphicMenu.SubMenu.Items.Add ( addGraphic );
+			graphicMenu.SubMenu.Items.Add ( editGraphic );
+			graphicMenu.SubMenu.Items.Add ( extractGraphic );
+			graphicMenu.SubMenu.Items.Add ( deleteGraphic );
 
 			var systemMenu = new MenuItem ( "_System" );
-			var newWindowSystem = new MenuItem ("New _window");
+			var newWindowSystem = new MenuItem ( "New _window" );
 			systemMenu.SubMenu = new Menu ();
 
-			systemMenu.SubMenu.Items.Add (newWindowSystem);
+			systemMenu.SubMenu.Items.Add ( newWindowSystem );
 
 			mainMenu.Items.Add ( fileMenu );
 			mainMenu.Items.Add ( editMenu );
+			mainMenu.Items.Add ( paletteMenu );
 			mainMenu.Items.Add ( graphicMenu );
 			mainMenu.Items.Add ( systemMenu );
 
-            MainMenu = mainMenu;
+			MainMenu = mainMenu;
 
-            var sampleLabel = new Label ( "Lalala" );
-            Content = sampleLabel;
-        }
+			var sampleLabel = new Label ( "Lalala" );
+			Content = sampleLabel;
+		}
 
-        protected override bool OnCloseRequested ()
-        {
-            var allow_close = MessageDialog.Confirm ( "FpgUI will be closed", Command.Ok );
-            if ( allow_close )
-                Application.Exit ();
-            return allow_close;
-        }
-    }
+		protected override bool OnCloseRequested ()
+		{
+			var allow_close = MessageDialog.Confirm ( "FpgUI will be closed", Command.Ok );
+			if ( allow_close )
+				Application.Exit ();
+			return allow_close;
+		}
+	}
 }
 
 
